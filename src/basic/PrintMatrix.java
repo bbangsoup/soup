c7e14d786e38a1507ba49fe765188bd70701f7ca
bluewithwindy <bluewithwindy@gmail.com>
2025-05-20 00:51:17 +0900

    3,6,9게임 완료

--- src/basic/PrintMatrix.java
+++ src/basic/PrintMatrix.java
@@ -3,20 +3,34 @@ package basic;
 public class PrintMatrix {
     public static void main(String[] args) {
 
-
+        // 3,6,9 게임
         for (int i=1; i<=100; i++)
         {
-            int ten = i / 10;
+            int sib = i / 10;
             int il = i % 10;
+            int count = 0;      // 뭔가를 추가로 더 따져야 할때.. // for 돌때마다 0으로 초기화
 
-            if (i%3 == 0)
-                System.out.print("짝\t");
-            else
-                System.out.printf("%d\t", i);
+            if (sib == 3 || sib == 6 || sib == 9)
+            {
+                System.out.print("짝");
+                count++;
+            }
+            if (il == 3 || il == 6 || il == 9)
+            {
+                System.out.print("짝");
+                count++;
+            }
+            if (count == 0)
+                System.out.print(i);
+
+            System.out.printf("\t");
+
             if (i%10 == 0)
                 System.out.println();
 
 
         }
+
+
     }
 }
