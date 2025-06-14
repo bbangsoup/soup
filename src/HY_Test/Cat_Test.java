bf837d0cab4f818d70ca84a5fe01c32256325b62
bluewithwindy <bluewithwindy@gmail.com>
2025-06-14 23:24:00 +0900

    25.06.14 review

---
+++ src/HY_Test/Cat_Test.java
@@ -0,0 +1,15 @@
+package HY_Test;
+
+public class Cat_Test {
+    public static void main(String[] args) {
+        Cat.count = 1000;
+        Cat c1 = new Cat("야옹이");
+        Cat c2 = new Cat("냥이");
+
+
+        System.out.println(Cat.count);
+
+
+
+    }
+}
